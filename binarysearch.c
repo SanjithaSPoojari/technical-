050fc64c4b9e381ec758cf5252c189e343d9323f
SanjithaSPoojari <118158064+SanjithaSPoojari@users.noreply.github.com>
2023-06-26 14:37:03 +0530

    Add files via upload

---
+++ binarysearch.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int binarySearch(int arr[],int key,int size){
+    for (int lb=0,ub=size-1,middle;lb<=ub;){
+        middle=(lb+ub)/2;
+        if(arr[middle]==key){
+            return middle;
+        }
+        else if(arr[middle]>key){
+            ub=middle-1;
+        }
+        else{
+            lb=middle+1;
+        }
+    }
+    return -1;
+}
+int main(){
+    int list[]={10,20,40,50,60,70,80};
+    int len=sizeof(list)/sizeof(list[0]),key=80;
+    int index=binarySearch(list,key,len);
+    if(index==-1)
+        printf("%d not present!",key);
+    else
+        printf("%d is found at %d",key,index+1);
+    return 0;
+}
