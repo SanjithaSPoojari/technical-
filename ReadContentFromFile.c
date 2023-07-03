c606651d82b276d2528cd6401bd2fc72716601bc
SanjithaSPoojari <118158064+SanjithaSPoojari@users.noreply.github.com>
2023-07-03 14:06:32 +0530

    Add files via upload

--- ReadContentFromFile.c
+++ ReadContentFromFile.c
@@ -1,24 +1,26 @@
-//program to read content from a file
+//program to read content from FILE
 #include<stdio.h>
-#include<stdlib.h>
-int main(){
-   FILE *fptr;
-   char fileName[30];
-   printf("Enter file name:");
-   scanf("%s",fileName);
-   //opening <filename> for writing content
-   fptr=fopen(fileName,"r");
-   //checking whether the file is created or not
-   if(fptr==NULL){
-       printf("%s does not exist!",fileName);
-       exit(0);
-   }
-   //file is created
-   while(1){
-       char ch=fgetc(fptr);
-      if(ch==EOF)break;
-      putc(ch,stdout);
-   }
-   fclose(fptr);
-   return 0;
+int main()
+{
+    FILE *fptr;
+    char filename[30];
+    printf("Enter file name:");
+    scanf("%s",filename);
+    //opening the file <filename> for reading
+    fptr=fopen(filename,"r");
+    //checking if the file does not exist
+    if(fptr==NULL){
+        printf("%s does not exist",filename);
+        return 0;
+    }
+    //file exist
+    while(!feof(fptr)){
+        char sentence[80];
+        //reading a line from the given file
+        fgets(sentence,80,fptr);
+        printf("%s",sentence);
+    }
+    //closing the file
+    fclose(fptr);
+    return 0;
 }
